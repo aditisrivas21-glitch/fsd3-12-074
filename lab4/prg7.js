ccdb2126c67b1671f0996b72748663aa259c259f
Aditi Srivastava <aditisrivas21@gmail.com>
2026-09-18 13:49:01 +0530

    add user

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,24 +1,32 @@
 import http from "http";
+import { addUser, getUsers } from "./users.js";
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
- 
   if (req.url === "/api/users" && req.method === "GET") {
-    res.end(JSON.stringify({ msg: "all users" }));
-  } else if (req.url === "/api/users" && req.method === "POST") {
-    res.end(JSON.stringify({ msg: "add user" }));
-  } else if (req.url === "/api/users/1" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
+  } else if (req.url === "/api/users" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added", userCreated }));
+    });
+  } else if (req.url.startsWith("/api/users/") && req.method === "GET") {
+    const userId = Number(req.url.split("/").pop());
+    res.end(
+      JSON.stringify({ msg: `Showing details of user with id ${userId}` }),
+    );
   } else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user 1" }));
   } else if (req.url === "/api/users/1" && req.method === "DELETE") {
     res.end(JSON.stringify({ msg: "remove 1" }));
   } else {
     res.statusCode = 404;
-    res.end(JSON.stringify({ msg: "Route not found" }));
+    res.end();
   }
 });
 
-server.listen(3000, () => {
-  console.log("prg7 is running...");
-});
+server.listen(3000, () => console.log("prg7 is running"));
--- lab4/users.js
+++ lab4/users.js
@@ -1,13 +1,25 @@
 // we use in memory database
 let users = [
-  { id: 1, name: "Ananya", mob: "748816****", email: "ananya@gmail.com" },
+  {
+    id: 1,
+    name: "Amit Sharma",
+    mob: "98345xxxxx",
+    email: "amit.example@exam.com",
+  },
   {
     id: 2,
-    name: "shivangi rai",
-    mob: "748815***",
-    email: "raishivangi@gmail.com",
+    name: "Monika Verma",
+    mob: "92345xxxxx",
+    email: "moni.example@exam.com",
   },
 ];
+
 let nextId = 3;
 
 export const getUsers = () => users;
+
+export const addUser = (user) => {
+  user.id = nextId++;
+  users.push(user);
+  return user;
+};
